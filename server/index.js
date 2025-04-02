9769e87a8955ee429061cbbdef97b2d37ed36fb4
Szymon <szymonjarosz102@gmail.com>
2025-04-02 20:08:07 +0200

    added cors back in

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
-// import cors from 'cors'
+import cors from 'cors'
 import mongoose from 'mongoose'
 import errorHandling from './middleware/errorHandling.js'
 import invalidRoute from './invalidRoute.js'
